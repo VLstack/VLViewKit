57f9d0c580aa36352abe6d345784c432462bc47c
VLstack <github@vlstack.fr>
2025-01-18 12:18:38 +0100

    typo

--- Sources/VLViewKit/extension.swift
+++ Sources/VLViewKit/extension.swift
@@ -5,8 +5,7 @@ extension View
  /// Conditionally hides this view.
  @inlinable nonisolated public func hidden(_ isHidden: Bool) -> some View
  {
-  self.frame(maxWidth: isHidden ? 0 : nil,
-             maxHeight: isHidden ? 0 : nil)
+  self.frame(maxWidth: isHidden ? 0 : nil, maxHeight: isHidden ? 0 : nil)
       .opacity(isHidden ? 0 : 1)
       .disabled(isHidden)
  }
