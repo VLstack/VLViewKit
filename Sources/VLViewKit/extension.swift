9864a1d1a75e0f1fab371ff79703a691cdf2a916
VLstack <github@vlstack.fr>
2025-01-19 09:27:43 +0100

    add visible(:)

--- Sources/VLViewKit/extension.swift
+++ Sources/VLViewKit/extension.swift
@@ -9,12 +9,18 @@ extension View
       .opacity(isHidden ? 0 : 1)
       .disabled(isHidden)
  }
- 
+
  /// Conditionally hides this view and optionally allows hit testing.
  @inlinable nonisolated public func hidden(_ isHidden: Bool,
                                            allowHitTesting: Bool) -> some View
  {
   self.hidden(isHidden)
       .allowsHitTesting(allowHitTesting)
+ }
+
+ /// Conditionally display this view.
+ @inlinable nonisolated public func visible(_ isVisible: Bool) -> some View
+ {
+  self.hidden(!isVisible)
  }
 }
