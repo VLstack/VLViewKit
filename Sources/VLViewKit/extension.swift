ce6e0f2b8946241d67158a7aa342d0f9e47c2b09
VLstack <github@vlstack.fr>
2025-01-19 09:33:15 +0100

    deprecate hidden(_:) renamed to invisible(_:)

--- Sources/VLViewKit/extension.swift
+++ Sources/VLViewKit/extension.swift
@@ -2,8 +2,21 @@ import SwiftUI
 
 extension View
 {
+ @available(*, deprecated, renamed: "invisible(_:)")
+ public func hidden(_ isHidden: Bool) -> some View
+ {
+  self.invisible(isHidden)
+ }
+
+ @available(*, deprecated, renamed: "invisible(_:allowHitTesting:)")
+ public func hidden(_ isHidden: Bool,
+                    allowHitTesting: Bool) -> some View
+ {
+  self.invisible(isHidden, allowHitTesting: allowHitTesting)
+ }
+
  /// Conditionally hides this view.
- @inlinable nonisolated public func hidden(_ isHidden: Bool) -> some View
+ @inlinable nonisolated public func invisible(_ isHidden: Bool) -> some View
  {
   self.frame(maxWidth: isHidden ? 0 : nil, maxHeight: isHidden ? 0 : nil)
       .opacity(isHidden ? 0 : 1)
@@ -11,16 +24,16 @@ extension View
  }
 
  /// Conditionally hides this view and optionally allows hit testing.
- @inlinable nonisolated public func hidden(_ isHidden: Bool,
+ @inlinable nonisolated public func invisible(_ isHidden: Bool,
                                            allowHitTesting: Bool) -> some View
  {
-  self.hidden(isHidden)
+  self.invisible(isHidden)
       .allowsHitTesting(allowHitTesting)
  }
 
  /// Conditionally display this view.
  @inlinable nonisolated public func visible(_ isVisible: Bool) -> some View
  {
-  self.hidden(!isVisible)
+  self.invisible(!isVisible)
  }
 }
